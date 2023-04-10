b92424e3689c671261e0891a0ff272668d531684
J-dotjs <jrb32002@gmail.com>
2023-04-10 17:25:56 -0400

    message command

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,7 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
 
 module.exports = {
-
-    data: new SlashCommandBuilder()
-
-const data = new ContextMenuCommandBuilder()
-	.setName('User Information')
-	.setType(ApplicationCommandType.User);
+     data: new ContextMenuCommandBuilder()
+	.setName('Override time')
+	.setType(ApplicationCommandType.Message)
+}
