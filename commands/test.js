445f3ac92287ca2f8f37f8606cf1fb4c8919f12b
J-dotjs <jrb32002@gmail.com>
2023-04-10 17:18:00 -0400

    dev

---
+++ commands/test.js
@@ -0,0 +1,7 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+
+module.exports
+
+const data = new ContextMenuCommandBuilder()
+	.setName('User Information')
+	.setType(ApplicationCommandType.User);
