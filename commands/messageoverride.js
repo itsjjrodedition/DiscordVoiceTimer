bdc165fcd5659733521a481692d1bcb677a1ecab
Jared <jrb32002@gmail.com>
2023-04-11 13:06:15 -0400

    message context menu command working

--- commands/messageoverride.js
+++ commands/messageoverride.js
@@ -1,10 +1,21 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
 
+const editJsonFile = require("edit-json-file");
+
+var file = editJsonFile(`${process.cwd()}/call.json`, {
+    autosave: true
+});
+
 module.exports = {
      data: new ContextMenuCommandBuilder()
-	.setName('Override time')
-	.setType(ApplicationCommandType.Message),
+	     .setName('Set time to this ts')
+	     .setType(ApplicationCommandType.Message),
      async execute(interaction, client){
-    	interaction.reply(interaction.targetId)
+          var message = await interaction.channel.messages.fetch(interaction.targetId);
+          console.log(message)
+          file.set("callStartTime", Date.parse(message.createdAt));
+          file.set("callDate", new Date(message.createdAt).toLocaleDateString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3"));
+          file.set("callTime", new Date(message.createdAt).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3"));
+          await interaction.reply({content: `The call's time has been overriden to \`${message.createdAt}\``, ephemeral: true});
      }
 }
